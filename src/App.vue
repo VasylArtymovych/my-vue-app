9075281bc88586b998fa13f3372d4e47235ee2ee
VasylArtymovych <art777vasss@gmail.com>
2023-03-11 08:21:36 +0200

    add start files

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,100 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="app">
+    <form class="form" @submit.prevent>
+      <h2>Creating post</h2>
+      <input
+        v-bind:value="title"
+        @input="inputTitle"
+        class="input"
+        type="text"
+        placeholder="title"
+      />
+      <input
+        v-bind:value="text"
+        @input="this.text = $event.target.value"
+        class="input"
+        type="text"
+        placeholder="description"
+      />
+      <button class="btn" @click="createPost">Create</button>
+    </form>
+    <div class="posts" v-for="post in posts">
+      <div><strong>Post title:</strong> {{ post.title }}</div>
+      <div><strong>Description:</strong> {{ post.text }}</div>
+    </div>
+  </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      posts: [
+        { id: 1, title: 'JavaScript', text: 'Some text' },
+        { id: 2, title: 'JavaScript 2', text: 'Some text 2' },
+        { id: 3, title: 'JavaScript 3', text: 'Some text 3' },
+        { id: 4, title: 'JavaScript 4', text: 'Some text 4' },
+      ],
+      title: '',
+      text: '',
+    };
+  },
+
+  methods: {
+    createPost(event) {
+      const post = {
+        id: new Date(),
+        title: this.title,
+        text: this.text,
+      };
+
+      this.posts.push(post);
+      this.title = '';
+      this.text = '';
+    },
+
+    inputTitle(event) {
+      this.title = event.target.value;
+    },
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-nav {
-  padding: 30px;
+.app {
+  padding: 20px;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.form {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+.input {
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+
+.btn {
+  align-self: flex-end;
+  padding: 10px;
+  border: 1px solid teal;
+  color: teal;
+  background-color: transparent;
+}
+
+.posts {
+  width: 100%;
+  padding: 10px;
+  border: 2px solid teal;
+  margin-bottom: 10px;
 }
 </style>
